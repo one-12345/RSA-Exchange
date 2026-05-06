2d2ba96aeda844156023ca0ee66fd9fff328bee2
shashwatgupta01 <shashwatgupta121608@gmail.com>
2026-05-05 21:55:09 -0400

    added to Bob and Alice

--- Alice.java
+++ Alice.java
@@ -1,3 +1,27 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
 public class Alice {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Alice side");
+        System.out.print("enter Bob n: ");
+        BigInteger n = new BigInteger(scanner.nextLine().trim());
+        System.out.print("enter Bob e: ");
+        BigInteger e = new BigInteger(scanner.nextLine().trim());
+        System.out.print("\nenter message for Bob: ");
+        String message = scanner.nextLine();
+        
+        BigInteger m = new BigInteger(message.getBytes());
+
+        //Encrypt c = m^e mod n
+        BigInteger c = RSA.modPower(m, e, n);
+
+        System.out.println("original: " + message);
+        System.out.println("m: " + m);
+        System.out.println("\nciphertext:");
+        System.out.println(c);
+
+        scanner.close();
+    }
 }
--- Bob.java
+++ Bob.java
@@ -1,3 +1,45 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
 public class Bob {
-    
+    public static void main(String[] args) {
+        System.out.println("Bob side");
+
+        BigInteger p = RSA.generatePrime(1024);
+        BigInteger q = RSA.generatePrime(1024);
+        BigInteger n = p.multiply(q);
+        BigInteger phi = (p.subtract(BigInteger.ONE)).multiply(q.subtract(BigInteger.ONE));
+        BigInteger e = BigInteger.valueOf(65537);
+        
+        while (!RSA.gcd(e, phi).equals(BigInteger.ONE)) {
+            e = e.add(BigInteger.valueOf(2));
+        }
+
+        BigInteger d = e.modInverse(phi);
+
+        System.out.println("prime p: " + p);
+        System.out.println("\nprime q: " + q);
+        System.out.println("\nn: " + n);
+        System.out.println("\nphi(n): " + phi);
+        System.out.println("\ne: " + e);
+        System.out.println("\nkey d: " + d);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("\nenter the ciphertext from Alice: ");
+        
+        String input = scanner.nextLine();
+        if (!input.isEmpty()) {
+            try {
+                BigInteger c = new BigInteger(input.trim());
+                
+                //Decrypt m = c^d mod n
+                BigInteger m = RSA.modPower(c, d, n);
+                
+                String decryptedMessage = new String(m.toByteArray());
+                System.out.println("\noriginal message: " + decryptedMessage);
+            } catch (Exception ex) {
+                System.out.println("Error invalid input");
+            }
+        }
+        scanner.close();
+    }
 }
